dd26be775ff8df5903d869ed7bb15ef8e54ede57
Dmitrii <pozdeev_dimon@mail.ru>
2021-08-31 04:11:40 +0500

    mod script

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 document.querySelector('h1').innerHTML = 'Hello!';
 console.log('Privet');
+
+function unknown(){
+    document.querySelector('h1').innerHTML = 'Hello!';
+    console.log('Privet');
+}
+
+console.log(unknown());
+
+for (let index = 0; index < 20; index++) {
+    let count =+ 1;
+}
+
+document.querySelector('h1').innerHTML = 'Hello!';
